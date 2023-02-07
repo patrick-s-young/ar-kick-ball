ae9074b9bed28933f2e83b81f59b96f326ecfae4
patrick-s-young <patrick.young@pokeratlas.com>
2023-02-06 17:57:45 -0800

    landscape UI and press-to-move direction buttons @WXR-005

--- src/components/DirectionMenu/index.js
+++ src/components/DirectionMenu/index.js
@@ -3,63 +3,79 @@
 export const DirectionMenu = ({ 
   menuParent, 
   setDirection,
+  setClipAction,
   camera }) => {
 
   const flexContainer = document.createElement('div');
-  flexContainer.style.display = 'inline';
-  flexContainer.style.position = 'absolute';
-  flexContainer.style.width = '100vw';
-  flexContainer.style.marginTop = '70vh';
-  flexContainer.style.justifyContent = 'center';
+  flexContainer.style.display = 'flex';
+  flexContainer.style.position = 'relative';
+  flexContainer.style.marginLeft = '12vw';
+  flexContainer.style.marginTop = '6vw';
+  flexContainer.style.width = '75vw';
+  flexContainer.style.height = '75vw';
   menuParent.appendChild(flexContainer);
 
   
-
+  //document.addEventListener('touchend', onStop);
 
   [
-    'spacer', 'TOP', 'spacer',
-    'LEFT', 'spacer', 'RIGHT',
-    'spacer', 'BOTTOM', 'spacer'].forEach(item => flexContainer.appendChild(navButton({ label: item })))
+    ['spacer', 'BOTTOM', 'spacer'],
+    ['LEFT', 'spacer', 'RIGHT'],
+    ['spacer', 'TOP', 'spacer']
+  ].forEach(item => flexContainer.appendChild(navRow(item)))
+
+
+  function navRow (items) {
+    const flexRow = document.createElement('div');
+    items.forEach(item => {
+      flexRow.appendChild(navButton({ label: item }))
+    });
+    return flexRow;
+  }
   
   
   function navButton ({ label }) {
     const div = document.createElement('div');
-    div.style.width = '33%';
-    div.style.height = '16vw';
+    div.style.display = 'flex';
+    div.style.width = '25vw';
+    div.style.height = '25vw';
+    div.style.borderRadius = '50%';
     div.style.backgroundColor = label === 'spacer' ? undefined : 'orange';
-    div.style.display = 'inline-block';
+    //div.style.display = 'inline-block';
     div.style.textAlign = 'center';
-    div.innerHTML = label === 'spacer' ? '&nbsp;' : label;
-    div.onclick = label === 'spacer' ? undefined : () => onClick({ label });
+    div.innerHTML = '&nbsp;';
+    div.id = label;
+    div.onpointerdown  = label === 'spacer' ? undefined : (ev) => onMove({ ev, label });
     return div;
   }
 
-  function onClick ({ label }) {
+  function onMove ({ ev, label }) {
     const [x, yNow, z, w] = camera.obj.quaternion.toArray();
-
-    // if (yNow !== yPrev) {
-       const angle = 2 * Math.acos(w);
-       let s;
-       if (1 - w * w < 0.000001) {
-         s = 1;
-       } else {
-         s = Math.sqrt(1 - w * w);
-       }
-       //const result = { axis: new THREE.Vector3(x/s, yNow/s, z/s), angle }
-       const cameraYradians = yNow/s * angle;
-       const yAngle = { 
-          TOP: cameraYradians,
-          RIGHT: cameraYradians - Math.PI/2,
-          BOTTOM: cameraYradians + Math.PI,
-          LEFT: cameraYradians + Math.PI/2
-       }
-
-       console.log('new Direction', yAngle[label] )
+    const angle = 2 * Math.acos(w);
+    let s;
+    if (1 - w * w < 0.000001) {
+      s = 1;
+    } else {
+      s = Math.sqrt(1 - w * w);
+    }
+    const cameraYradians = yNow/s * angle;
+    const yAngle = { 
+      TOP: cameraYradians,
+      RIGHT: cameraYradians - Math.PI/2,
+      BOTTOM: cameraYradians + Math.PI,
+      LEFT: cameraYradians + Math.PI/2
+    }
     setDirection(yAngle[label] );
+    setClipAction('Walk')
+  }
+
+  function onStop() {
+    setClipAction('Idle');
   }
 
 
   return {
-    
+    onStop,
+    get domElement() { return flexContainer}
   }
 }
