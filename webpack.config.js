162d7ef9199bdbe188b6b82c8915d794b8cebf3e
Patrick S. Young <patrick.young@pokeratlas.com>
2023-10-30 05:12:59 -0700

    start debug webpack script @KB-103

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,38 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  mode: 'development',
-  entry: { 
-    index: './src/index.js'
-  },
-  devtool: 'inline-source-map',
-  devServer: {
-    static: './dist',
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Development',
-    }),
-  ],
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true
-  },
-  optimization: {
-    runtimeChunk: 'single',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      }
+module.exports = (env) => ({
+    mode: 'development',
+    entry: { 
+      index: env.debug === 'true' ? './src/index.debug.js' : './src/index.js'
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+      static: './dist',
+    },
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Development',
+      })
     ],
-  }
-};
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true
+    },
+    optimization: {
+      runtimeChunk: 'single',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        }
+      ],
+    }
+});
